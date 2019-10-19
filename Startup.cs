f520bea7305b47097afce3a95bd73d3bf88c2c41
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-19 06:04:11 -0600

    setupd, dependency injection, packages needed

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,39 +12,80 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
+using skiLift.Repositories;
 
 namespace skiLift
 {
-    public class Startup
+  public class Startup
+  {
+    string _connectionString;
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
+      _connectionString = configuration.GetSection("DB").GetValue<string>("mySQLConnectionString");
     }
+
+    public IConfiguration Configuration { get; }
+    private IDbConnection CreateDBContext()
+    {
+      var connection = new MySqlConnection(_connectionString);
+      connection.Open();
+      return connection;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      //ADD USER AUTH through JWT
+      services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+          options.LoginPath = "/Account/Login";
+          options.Events.OnRedirectToLogin = (context) =>
+                  {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                  };
+        });
+      services.AddCors(options =>
+       {
+         options.AddPolicy("CorsDevPolicy", builder =>
+               {
+                 builder
+                           .WithOrigins(new string[]{
+                                "http://localhost:8080"
+                       })
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials();
+               });
+       });
+
+      services.AddMvc();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      //Register Repos
+
+      services.AddTransient<IDbConnection>(x => CreateDBContext());
+      services.AddTransient<UserRepository>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
 }
