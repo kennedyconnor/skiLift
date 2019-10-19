e5710de888db4e60d1f3a6dd0f01b19906a9e06f
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-19 04:23:28 -0600

    init commit

---
+++ wwwsrc/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home ">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    props: [],
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+<style>
+
+</style>
