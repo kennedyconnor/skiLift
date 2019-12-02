f28fa18bdc9138ffb6fe487bf7441517575a36ca
Connor Kennedy <kennedyconnor02@gmail.com>
2019-12-01 23:35:33 -0700

    creating components

---
+++ wwwsrc/src/components/GoogleMap.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class=" ">
+       
+    </div>
+</template>
+
+<script>
+export default {
+   name: "",
+   props: [],
+   data() {
+      return {}
+   },
+   computed: {},
+   methods: {},
+   components: {}
+}
+</script>
+
+<style>
+
+</style>
