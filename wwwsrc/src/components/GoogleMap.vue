b1604c754d254bfa8163a7cf9b041bac0f4445eb
Connor Kennedy <kennedyconnor02@gmail.com>
2020-01-25 23:43:56 -0700

    marker info

--- wwwsrc/src/components/GoogleMap.vue
+++ wwwsrc/src/components/GoogleMap.vue
@@ -33,9 +33,23 @@
             icon: {
                url: "ski.png", // url
                scaledSize: new google.maps.Size(30, 30),
-            }
-
+            },
+            //label: "Connor Kennedy"
          })
+
+         let contentString = '<div id="content">' +
+            '<div id="bodyContent">' +
+            '<ul>Name: Connor Kennedy</ul>' +
+            '<ul>Destination: Bogus Basin</ul>' +
+            '</div>' +
+            '</div>';
+
+         let infoWindow = new google.maps.InfoWindow({
+            content: contentString
+         })
+         marker.addListener('click', function () {
+            infoWindow.open(this.map, marker);
+         });
       }
    }
 </script>
