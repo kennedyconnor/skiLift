5f8107801c7d3e69fd0f3e689df860c00e470f9b
Connor Kennedy <kennedyconnor02@gmail.com>
2020-01-26 23:56:39 -0700

    ch ch ch ch changes

--- wwwsrc/src/components/PassengerForm.vue
+++ wwwsrc/src/components/PassengerForm.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="passengerForm col-4">
-    <h3>You driving?</h3>
+    <h3>Passenger</h3>
+    <h5>Find a ride</h5>
+    <form>
+      <div class="form-group">
+        <label>Destination (Idaho)</label>
+        <select class="form-control" id="PassengerFormDestinationSelect">
+          <option>Bogus Basin</option>
+          <option>Tamarack</option>
+          <option>Brundage</option>
+          <option>Sun Valley</option>
+        </select>
+      </div>
+    </form>
   </div>
 </template>
 
--- wwwsrc/src/components/RideForm.vue
+++ wwwsrc/src/components/RideForm.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="rideForm col-4 offset-2">
-    <h3>Are you looking to get a ride?</h3>
+    <h3>Driver</h3>
+    <h5>Pick up passengers</h5>
+    <form>
+      <div class="form-group">
+        <label>Destination (Idaho)</label>
+        <select class="form-control" id="RideFormDestinationSelect">
+          <option>Bogus Basin</option>
+          <option>Tamarack</option>
+          <option>Brundage</option>
+          <option>Sun Valley</option>
+        </select>
+      </div>
+    </form>
   </div>
 </template>
 
