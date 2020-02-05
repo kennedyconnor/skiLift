8e78218a0af3158976bc169bd3cc8600ba93cb32
Connor Kennedy <kennedyconnor02@gmail.com>
2020-02-04 23:40:38 -0700

    login form

--- wwwsrc/src/components/Login.vue
+++ wwwsrc/src/components/Login.vue
@@ -11,7 +11,22 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Enter Login Information</h5>
+            <form v-if="loginForm" @submit.prevent="loginUser">
+              <input type="email" v-model="creds.email" placeholder="email">
+              <input type="password" v-model="creds.password" placeholder="password">
+              <button type="submit">Login</button>
+            </form>
+            <form v-else @submit.prevent="register">
+              <input type="text" v-model="newUser.username" placeholder="name">
+              <input type="email" v-model="newUser.email" placeholder="email">
+              <input type="password" v-model="newUser.password" placeholder="password">
+              <button type="submit">Create Account</button>
+            </form>
+            <div @click="loginForm = !loginForm">
+              <p v-if="loginForm">No account Click to Register</p>
+              <p v-else>Already have an account click to Login</p>
+            </div>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -21,7 +36,7 @@
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-primary">Submit</button>
           </div>
         </div>
       </div>
@@ -35,8 +50,16 @@
     props: [],
     data() {
       return {
-        username: "",
-        password: ""
+        loginForm: true,
+        creds: {
+          email: "",
+          password: ""
+        },
+        newUser: {
+          email: "",
+          password: "",
+          username: ""
+        }
       }
     },
     computed: {},
